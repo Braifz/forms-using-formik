842bc683f15c3ab0831f03ce6832ddab20db7713
Braifz <braian94lp@gmail.com>
2021-12-09 19:18:42 -0300

    forms using formik

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik,Form, ErrorMessage } from "formik";
+import Checkbox from "./components/Checkbox";
+import TextInput from './components/TextInput'
+import Select from './components/Select'
+import Radio from './components/Radio'
+
+const validate = (values) => {
+  const errors = {}
+
+  if(!values.name){
+    errors.name = 'Requerido'
+  } else if (values.name.length < 5) {
+    errors.name = "El nombre es muy corto"
+  }
+
+  if(!values.lastname){
+    errors.lastname = 'Requerido'
+  } else if (values.lastname.length < 5) {
+    errors.lastname = "El apellido es muy corto"
+  }
+
+  if (!values.radio) {
+    errors.radio = "Requerido"
+  }
+  return errors;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Formik
+      initialValues={
+        {
+        name: "",
+        lastname: "",
+        email: "",
+        chancho: "",
+        radio:"",
+        }
+      }
+      validate={validate}
+      onSubmit={values => console.log(values)}
+      >
+      <Form>
+          <TextInput name="name" label="Nombre"/>
+          <br/>
+          <TextInput name="lastname" label="Apellido"/>
+          <br />
+          <TextInput name="email" label="Email"/>
+        <br />
+        <Select label="Tipo de chancho" name="chancho">
+          <option value="" >Seleccione chancho</option>
+          <option value="felipe" >Felipe</option>
+          <option value="chanchitofeliz" >Chanchito feliz</option>
+          <option value="chanchito triste" >Chanchito triste</option>
+        </Select>
+          <Checkbox name="accept">
+            Aceptar terminos y condiciones
+        </Checkbox>
+          <Radio name="radio" value="chanchito1" label="chanchito 1"/>
+          <Radio name="radio" value="chanchito2" label="chanchito 2"/>
+        <Radio name="radio" value="chanchito3" label="chanchito 3" />
+        <ErrorMessage name="radio"/>
+          <br />
+          <button type="submit">Enviar</button>
+          </Form>
+      </Formik>
+    );
+  }
 
 export default App;
